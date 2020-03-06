3a01efc0e03907fa6ccd1efa70d60ba663b1d061
Rafal Wojciechowski <wojciechowskirafal0@gmail.com>
2020-03-06 11:43:11 +0100

    Almost final version,
    there is only one problem that i need to solve.
    In the last round if player or cpu won system print message "CAT" like there was a tie.

--- src/Game.java
+++ src/Game.java
@@ -1,54 +1,53 @@
 import java.util.*;
 
-public class Game extends GameBoard{
+public class Game extends GameBoard {
 
     private ArrayList<Integer> playerPosition = new ArrayList<>();
     private ArrayList<Integer> cpuPosition = new ArrayList<>();
+    private Random random = new Random();
+    private Scanner scanner = new Scanner(System.in);
 
-    private  Scanner scanner = new Scanner(System.in);
 
-    private int position;
-
-    public void markPosition(char [] [] gameBoard, int position, String user){
-
-        this.position=position;
+    public void markPosition(char[][] gameBoard, int position, String user) {
 
         char symbol = 'X';
 
-        if(user.equals("cpu")){
+        if (user.equals("cpu")) {
             symbol = 'O';
             cpuPosition.add(position);
+        } else {
+            playerPosition.add(position);
         }
-        playerPosition.add(position);
+
 
         this.gameBoard = gameBoard;
-        switch (position){
+        switch (position) {
             case 1:
-                gameBoard [0] [0] = symbol;
+                gameBoard[0][0] = symbol;
                 break;
             case 2:
-                gameBoard [0] [2] = symbol;
+                gameBoard[0][2] = symbol;
                 break;
             case 3:
-                gameBoard [0] [4] = symbol;
+                gameBoard[0][4] = symbol;
                 break;
             case 4:
-                gameBoard [2] [0] = symbol;
+                gameBoard[2][0] = symbol;
                 break;
             case 5:
-                gameBoard [2] [2] = symbol;
+                gameBoard[2][2] = symbol;
                 break;
             case 6:
-                gameBoard [2] [4] = symbol;
+                gameBoard[2][4] = symbol;
                 break;
             case 7:
-                gameBoard [4] [0] = symbol;
+                gameBoard[4][0] = symbol;
                 break;
             case 8:
-                gameBoard [4] [2] = symbol;
+                gameBoard[4][2] = symbol;
                 break;
             case 9:
-                gameBoard [4] [4] = symbol;
+                gameBoard[4][4] = symbol;
                 break;
             default:
                 break;
@@ -56,18 +55,37 @@ public class Game extends GameBoard{
         printGameBoard(gameBoard);
     }
 
-    public void start(){
-        while(hasEmptySpace()){
-            position = scanner.nextInt();
+    public void start() {
+        while (hasEmptySpace()) {
+
+
+            int position = scanner.nextInt();
+
+            while(playerPosition.contains(position) || cpuPosition.contains(position)){
+                System.out.println("Position taken, select another position");
+                position = scanner.nextInt();
+            }
+
+
             markPosition(gameBoard, position, "player");
             System.out.println();
-            Random random = new Random();
-            int cpuPos = random.nextInt(9)+1;
+            if (checkWinner()) {
+                break;
+            }
+
+           int  cpuPos = random.nextInt(9) + 1;
+            while(playerPosition.contains(cpuPos) || cpuPosition.contains(cpuPos)){
+                cpuPos = random.nextInt(9) + 1;
+            }
             markPosition(gameBoard, cpuPos, "cpu");
+            System.out.println();
+            if (checkWinner()) {
+                break;
+            }
         }
     }
 
-    public String checkWinner() {
+    public boolean checkWinner() {
 
         List topRow = Arrays.asList(1, 2, 3);
         List midRow = Arrays.asList(4, 5, 6);
@@ -78,7 +96,7 @@ public class Game extends GameBoard{
         List cross1 = Arrays.asList(1, 5, 9);
         List cross2 = Arrays.asList(7, 5, 3);
 
-        List winning = new ArrayList<List>();
+        List<List> winning = new ArrayList<>();
 
         winning.add(topRow);
         winning.add(midRow);
@@ -89,7 +107,18 @@ public class Game extends GameBoard{
         winning.add(cross1);
         winning.add(cross2);
 
-        
-        return " ";
+        for (List l : winning) {
+            if (playerPosition.containsAll(l)) {
+                System.out.println("Congratulation You won!!!");
+                return true;
+            } else if (cpuPosition.containsAll(l)) {
+                System.out.println("You lose :( try again.");
+                return true;
+            } else if (!playerPosition.containsAll(l) && !cpuPosition.containsAll(l) && !hasEmptySpace()) {
+                System.out.println("CAT");
+                return true;
+            }
+        }
+        return false;
     }
 }
